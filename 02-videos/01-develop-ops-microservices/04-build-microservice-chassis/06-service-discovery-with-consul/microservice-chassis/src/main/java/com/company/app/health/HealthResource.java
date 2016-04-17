45e3e06030279349060d7c7b2c74fe188201617a
William Han <whan@godaddy.com>
2016-04-17 10:20:16 -0700

    finish service discovery with consul

---
+++ 02-videos/01-develop-ops-microservices/04-build-microservice-chassis/06-service-discovery-with-consul/microservice-chassis/src/main/java/com/company/app/health/HealthResource.java
@@ -0,0 +1,17 @@
+package com.company.app.health;
+
+import com.google.common.collect.ImmutableMap;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import java.util.Map;
+
+@Path("health")
+public class HealthResource {
+    @GET
+    @Produces("application/json")
+    public Map<String, Boolean> health(){
+        return ImmutableMap.of("healthy", true);
+    }
+}
