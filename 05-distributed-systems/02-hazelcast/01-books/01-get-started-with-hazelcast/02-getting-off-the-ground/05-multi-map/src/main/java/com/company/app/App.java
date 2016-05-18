154f9e80771328881f9926ea39e6e349358f2eff
William Han <whan@godaddy.com>
2016-05-18 11:42:17 -0700

    add multi map

---
+++ 05-distributed-systems/02-hazelcast/01-books/01-get-started-with-hazelcast/02-getting-off-the-ground/05-multi-map/src/main/java/com/company/app/App.java
@@ -0,0 +1,58 @@
+package com.company.app;
+
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+import com.hazelcast.core.MultiMap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// need to wait a little to the message is shown in console.
+public class App
+{
+    public static void main( String[] args )
+    {
+        HazelcastInstance hz = Hazelcast.newHazelcastInstance();
+        IMap<String, List<String>> manualCities = hz.getMap("manualCities");
+        List<String> gbCities = new ArrayList<String>();
+
+        manualCities.put("GB", gbCities);
+
+        gbCities = manualCities.get("GB");
+        gbCities.add("London");
+        manualCities.put("GB", gbCities);
+
+        gbCities = manualCities.get("GB");
+        gbCities.add("Southampton");
+        manualCities.put("GB", gbCities);
+
+        List<String> frCities = new ArrayList<String>();
+        manualCities.put("FR", frCities);
+
+        frCities = manualCities.get("FR");
+        frCities.add("Paris");
+        manualCities.put("FR", frCities);
+
+        System.out.println("HERE_1");
+
+        System.err.println(
+                String.format("Manual: GB=%s, FR=%s",
+                        manualCities.get("GB"),
+                        manualCities.get("FR")));
+
+
+        MultiMap<String, String> multiMapCities = hz.getMultiMap("multiMapCities");
+
+        multiMapCities.put("GB", "London");
+        multiMapCities.put("GB", "Southampton");
+
+        multiMapCities.put("FR", "Paris");
+
+        System.out.println("HERE_2");
+        System.err.println(
+                String.format("MultiMap: GB=%s, FR=%s",
+                        multiMapCities.get("GB"),
+                        multiMapCities.get("FR")));
+    }
+}
