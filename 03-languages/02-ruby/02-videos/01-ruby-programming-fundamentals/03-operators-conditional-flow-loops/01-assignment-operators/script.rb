4a46dab385562d27d9d96c5e589c71f0cd488a79
William Han <whan@godaddy.com>
2016-12-08 08:47:53 -0800

    add assignment

---
+++ 03-languages/02-ruby/02-videos/01-ruby-programming-fundamentals/03-operators-conditional-flow-loops/01-assignment-operators/script.rb
@@ -0,0 +1,30 @@
+my_number = 100
+
+puts "** addition **"
+puts my_number = my_number + 1 # 101
+puts my_number += 1 # 102
+
+puts "** multiplication **"
+my_number = 100
+puts my_number = my_number * 2 # 200
+puts my_number *= 2 # 400
+
+puts "** modulo  **"
+my_number = 2
+puts my_number %= 5 # 2
+
+puts "** exponent **"
+my_number = 10
+puts my_number **=2 # 100
+
+# output
+# ** addition **
+#     101
+# 102
+# ** multiplication **
+#     200
+# 400
+# ** modulo  **
+#     2
+# ** exponent **
+#     100
