b28dba184b5d7e639094240ac309df8deef8f8c1
William Han <whan@godaddy.com>
2017-04-20 21:58:38 -0700

    update .gitignore

---
+++ 21-languages/01-python/02-design-patterns/03-github/README.md
@@ -0,0 +1,3 @@
+# python-patterns
+
+https://github.com/faif/python-patterns
--- 21-languages/01-python/03-framework/01-django/01-try-django/trydjango18/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/whan/Dropbox/github/python-repo/10-1/python/03-framework/01-django/01-try-django/trydjango18/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
